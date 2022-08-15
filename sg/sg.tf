b1893942c8778339211cf70770ccfa6518d0a004
Ope Dare <dreoly57@gmail.com>
2022-08-14 21:49:21 -0600

    Module updated

--- sg/sg.tf
+++ sg/sg.tf
@@ -2,36 +2,22 @@ resource "aws_security_group" "ec2_sg" {
   name        = var.sg_name
   description = "Allow TLS inbound traffic"
   vpc_id      = var.vpc
-
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = var.cidr_blocks_id
-    ipv6_cidr_blocks = ["::/0"]
-
+  dynamic "ingress" {
+    for_each = [80, 22]
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = var.cidr_blocks_id
+    }
   }
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = var.cidr_blocks_id
-    ipv6_cidr_blocks = ["::/0"]
-
-  }
-
-  }
+}
 
 resource "aws_security_group" "rds_sg" {
   name        = var.sg_name1
   description = "Allow TLS inbound traffic"
   vpc_id      = var.vpc
-
-
   ingress {
     description      = "TLS from VPC"
     from_port        = 3306
