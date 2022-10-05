76d62b2d32356af88980d20780df851ef1a80e36
Ope Dare <dreoly57@gmail.com>
2022-10-05 15:49:56 -0600

    updated with s3 and policy

---
+++ Policies/policy.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket_policy " "s3-policy" {
+  
+}
---
+++ S3/s3.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "dre_s3" {
+    bucket = "real-8402"
+      
+}
+
+resource "aws_s3_bucket_website_configuration" "dre-website" {
+    bucket = aws_s3_bucket.dre_s3.bucket
+  
+}
