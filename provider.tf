691cb520f627d8f477cd862f6c1e423060026bab
Ope Dare <dreoly57@gmail.com>
2022-12-12 02:29:21 -0700

    provider adjusted

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
     region = "us-east-1"
-    aws_access_key_id = "AKIAXECZOZZUBLTOZPMP"
-    aws_secret_access_key = "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"
+    profile = "default"
 }
